c6768b48b1ec0d7cd5e7647cb2585b2a80d0cce4
Utku Keskin <utkukeskin@Utku-MacBook-Pro.local>
2025-06-01 19:52:26 +0300

    Update baseURL to use VITE_API_URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { toast } from 'react-toastify';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5038/api',
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
 });
 
 api.interceptors.request.use((config) => {
